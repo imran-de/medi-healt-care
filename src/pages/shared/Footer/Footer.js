8bce6c37d050c2135e135bb9218933ca50f61836
imran-de <stimran4405@gmail.com>
2021-10-19 22:05:17 +0600

    footer update

--- src/pages/shared/Footer/Footer.js
+++ src/pages/shared/Footer/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
     return (
@@ -11,59 +12,86 @@ const Footer = () => {
                     {/* <!-- Col-1 --> */}
                     <div className="col-md-3">
                         {/* <!-- Col Title --> */}
-                        <div className="">
-                            Getting Started
-                        </div>
+                        <h2 className="">
+                            Get In Touch
+                        </h2>
 
-                        {/* <!-- Links --> */}
+                        <div className="pt-4">
+                            <p>House: 06, Road: 08, Khadim, Sylhet</p>
+                            <p>Phone: +8801926-294263</p>
+                            <p>Email: stimran4405@gmail.com</p>
+                        </div>
 
                     </div>
 
                     {/* <!-- Col-2 --> */}
                     <div className="col-md-3">
                         {/* <!-- Col Title --> */}
-                        <div className="">
-                            Core Concepts
-                        </div>
+                        <h2 className="">
+                            Quick Links
+                        </h2>
 
-                        {/* <!-- Links --> */}
+                        <div className="pt-4">
+                            <ul class="nav flex-column">
+                                <li className="nav-item">
+                                    <Link className="nav-link active" to="/">Home</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/services">Services</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/about">About</Link>
+                                </li>
+                                <li className="nav-item">
+                                    <Link className="nav-link" to="/contact-us">Contact</Link>
+                                </li>
+                            </ul>
+                        </div>
 
                     </div>
 
                     {/* <!-- Col-3 --> */}
                     <div className="col-md-3">
                         {/* <!-- Col Title --> */}
-                        <div className="">
-                            Customization
-                        </div>
+                        <h2 className="">
+                            News
+                        </h2>
 
-                        {/* <!-- Links --> */}
+                        <div className="">
+                            <ul className="list-group list-group-flush">
+                                <li className="list-group-item bg-dark text-light border-bottom border-light">News Title</li>
+                                <li className="list-group-item bg-dark text-light border-bottom border-light">News Title</li>
+                                <li className="list-group-item bg-dark text-light border-bottom border-light">News Title</li>
+                                <li className="list-group-item bg-dark text-light border-bottom border-light">News Title</li>
+                                <li className="list-group-item bg-dark text-light border-bottom border-light">News Title</li>
+                            </ul>
+                        </div>
 
                     </div>
 
                     {/* <!-- Col-3 --> */}
                     <div className="col-md-3">
                         {/* <!-- Col Title --> */}
-                        <div className="">
-                            Community
+                        <h2 className="">
+                            Download Apps
+                        </h2>
+                        <div className="pt-4 text-center">
+                            <img className="w-100" src="https://instabridge.com/wp-content/themes/ib17/assets/img/badges/google-play-badge-en.svg" alt="" /><br /><br />
+                            <img src="https://instabridge.com/wp-content/themes/ib17/assets/img/badges/app-store-badge-en.svg" className="w-100" alt="" />
                         </div>
-
-                        {/* <!-- Links --> */}
 
                     </div>
                 </div>
 
                 {/* <!-- Copyright Bar --> */}
                 <div className="pt-2">
-                    <div className="flex pb-5 px-3 m-auto pt-5 
-            border-t border-gray-500 text-gray-400 text-sm 
-            flex-col md:flex-row max-w-6xl">
+                    <div className="">
                         <div className="mt-2">
-                            © Copyright 1998-year. All Rights Reserved.
+                            ©Medi-health Care Copyright 1996-2021. All Rights Reserved. Developed by <span className="text-info">S Imran Ahmed ST</span>
                         </div>
 
                         {/* <!-- Required Un icons (if you want) --> */}
-                        <div className="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+                        <div className="mt-3">
                             <a href="#home" className="w-6 mx-1">
                                 <i className="uil uil-facebook-f"></i>
                             </a>
